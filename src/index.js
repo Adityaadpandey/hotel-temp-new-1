8f2320c58d0b7d495b41dccf800917c8e15e034f
adityapandey <adityapandeyadp@gmail.com>
2023-04-23 21:21:53 +0530

    some more changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import{ inject } from '@vercel/analytics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+inject();
 root.render(
   <React.StrictMode>
     <App />
