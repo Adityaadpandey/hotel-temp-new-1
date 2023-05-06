049bae9911d4ef88509bc5f3cc8f6fa7bcbb0433
adityapandey <adityapandeyadp@gmail.com>
2023-05-06 21:06:10 +0530

    check

--- src/components/Google.jsx
+++ src/components/Google.jsx
@@ -5,9 +5,9 @@ import { GoogleLogin } from "@react-oauth/google";
 const google = () => {
   return (
     <GoogleLogin
-      onSuccess={(credentialResponse) => {
-        console.log(credentialResponse);
-        localStorage.setItem("cred", credentialResponse);
+      onSuccess={(userInfo) => {
+        console.log(userInfo);
+        localStorage.setItem("cred", userInfo);
         const cred = localStorage.getItem("cred");
         console.log(cred + "  Login Success");
 
@@ -15,6 +15,9 @@ const google = () => {
       onError={() => {
         console.log("Login Failed");
       }}
+      scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"
+      responseType="code"
+      accessType="offline"
     />
   );
 };
